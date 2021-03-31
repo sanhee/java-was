e693f51adbd0e73ff273054540a7281c1daf14af
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-31 17:31:15 +0900

    refactor: RequestHandler Response 객체를 이용하도록 수정
    직접 write하지 않고, 메세지를 보내면 Response에서 보내주도록 수정.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,12 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-import java.util.stream.Collectors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,35 +23,14 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String message = "HTTP/1.1 200 OK" + System.lineSeparator() +
+                    "Content-Type: text/html;charset=utf-8" + System.lineSeparator() +
+                    "Content-Length: " + "Hello World".getBytes().length + System.lineSeparator() +
+                    System.lineSeparator() +
+                    "Hello World";
 
-            log.debug("Request Message:"+ br.lines().collect(Collectors.joining(System.lineSeparator())));
-
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            Response response = Response.from(message);
+            response.write(out);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
---
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -0,0 +1,61 @@
+package webserver;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RequestHandlerTest {
+    @ParameterizedTest
+    @MethodSource("run")
+    void run(String message, String expectedResponseMessage) throws IOException {
+        int port = ThreadLocalRandom.current().nextInt(50000, 60000);
+        ServerSocket server = new ServerSocket(port);
+
+        Socket browser = new Socket("localhost", port);
+
+        RequestHandler requestHandler = new RequestHandler(server.accept());
+
+        BufferedOutputStream browserStream = new BufferedOutputStream(browser.getOutputStream());
+
+
+        browserStream.write(message.getBytes(StandardCharsets.UTF_8));
+        browserStream.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+        browserStream.flush();
+
+        requestHandler.start();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(browser.getInputStream()));
+
+        assertThat(br.lines().collect(Collectors.joining(System.lineSeparator()))).isEqualTo(expectedResponseMessage);
+    }
+
+    static Stream<Arguments> run() {
+        return Stream.of(
+                Arguments.arguments(
+                        "GET /index.html HTTP/1.1" + System.lineSeparator() +
+                                "Host: localhost:8080" + System.lineSeparator() +
+                                "Connection: keep-alive" + System.lineSeparator() +
+                                "Accept: */*" + System.lineSeparator() +
+                                "" + System.lineSeparator(),
+                        "HTTP/1.1 200 OK" + System.lineSeparator() +
+                                "Content-Type: text/html;charset=utf-8" + System.lineSeparator() +
+                                "Content-Length: 11" + System.lineSeparator() +
+                                "" + System.lineSeparator() +
+                                "Hello World"
+                )
+        );
+    }
+}
