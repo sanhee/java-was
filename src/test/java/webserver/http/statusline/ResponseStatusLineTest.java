bcc20a5fb037b871d55bf7f7be6e9f86ffbfb009
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-08-12 12:53:58 +0900

    test: ResponseStatusLine에서 protocol확인하는 테스트 추가

--- src/test/java/webserver/http/statusline/ResponseStatusLineTest.java
+++ src/test/java/webserver/http/statusline/ResponseStatusLineTest.java
@@ -58,4 +58,28 @@ class ResponseStatusLineTest {
                 )
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("protocol")
+    void protocol(String desc, ResponseStatusLine requestStatusLine, String expectedMethod) {
+        assertThat(requestStatusLine.protocol())
+                .as("status line에서 protocol 가져오기 : %s", desc)
+                .isEqualTo(expectedMethod);
+    }
+
+    static Stream<Arguments> protocol() {
+        return Stream.of(
+                Arguments.of(
+                        "HTTP/1.1",
+                        new ResponseStatusLine(
+                                new HashMap<String, String>() {{
+                                    put("protocolVersion", "HTTP/1.1");
+                                    put("statusCode", "200");
+                                    put("statusText", "OK");
+                                }}
+                        ),
+                        "HTTP/1.1"
+                )
+        );
+    }
 }
