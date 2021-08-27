c198b5e7799ac9a116c0dca2a9d7f38e60c9617b
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-08-27 21:44:47 +0900

    refactor: RequestHandlerTest에서 사용할 파일 캐싱해두는 클래스 생성
    new File을 이용해 미리 생성해둠. Files.lines는 예외처리가 까다로워
    이번에는 제외함. 추후 메소드를 이용해볼 수 있을듯.

---
+++ src/test/java/webserver/FileForTest.java
@@ -0,0 +1,8 @@
+package webserver;
+
+import java.io.File;
+
+public class FileForTest {
+    public static final File INDEX_HTML = new File("./webapp/index.html");
+    public static final File FORM_HTML = new File("./webapp/index.html");
+}
