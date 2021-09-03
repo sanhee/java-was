2c1ce14c76d5c7be6a8e664415a4f566d4c3617d
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-09-03 18:07:54 +0900

    fix: FileForTest의 잘못된 File 경로 수정
    FORM_HTML이 index.html 가리키고 있는 부분 수정

--- src/test/java/webserver/FileForTest.java
+++ src/test/java/webserver/FileForTest.java
@@ -4,5 +4,5 @@ import java.io.File;
 
 public class FileForTest {
     public static final File INDEX_HTML = new File("./webapp/index.html");
-    public static final File FORM_HTML = new File("./webapp/index.html");
+    public static final File FORM_HTML = new File("./webapp/user/form.html");
 }
