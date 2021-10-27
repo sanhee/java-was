562ce02a55eb0b0c8bc5c36b40e158188342b64e
sanhee <68936@naver.com>
2021-10-27 18:45:45 +0900

    feat: RequestHeader Attribute(일급컬렉션)으로 생성 가능하게 정의

--- src/main/java/webserver/http/header/RequestHeader.java
+++ src/main/java/webserver/http/header/RequestHeader.java
@@ -1,6 +1,7 @@
 package webserver.http.header;
 
 import util.HttpRequestUtils;
+import webserver.http.Attribute;
 import webserver.http.statusline.RequestStatusLine;
 
 import java.util.List;
@@ -14,7 +15,16 @@ public class RequestHeader extends Header {
         this.statusLine = statusLine;
     }
 
+    protected RequestHeader(RequestStatusLine statusLine, Attribute attributes) {
+        super(attributes);
+        this.statusLine = statusLine;
+    }
+
     public static RequestHeader of(List<String> statusLine, Map<String, String> attributes) {
+        return new RequestHeader(RequestStatusLine.from(statusLine), attributes);
+    }
+
+    public static RequestHeader of(List<String> statusLine, Attribute attributes) {
         return new RequestHeader(RequestStatusLine.from(statusLine), attributes);
     }
 
@@ -23,6 +33,13 @@ public class RequestHeader extends Header {
         List<String> statusLine = HttpRequestUtils.parseStatusLine(splittedHeaderTexts[0]);
 
         return RequestHeader.of(statusLine, attributeFrom(headerText));
+    }
+
+    public static RequestHeader newFrom(String headerText) {
+        String[] splittedHeaderTexts = headerText.split(System.lineSeparator());
+        List<String> statusLine = HttpRequestUtils.parseStatusLine(splittedHeaderTexts[0]);
+
+        return RequestHeader.of(statusLine, attributeNewFrom(headerText));
     }
 
     public String getPath() {
