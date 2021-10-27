cbcda5c8e0c927878e1ce745e47e7f7f773b7c12
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-10-27 18:28:59 +0900

    feat: from 및 equals & hashcode 추가

--- src/main/java/webserver/http/attribute/Attributes.java
+++ src/main/java/webserver/http/attribute/Attributes.java
@@ -1,9 +1,23 @@
 package webserver.http.attribute;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Attributes {
     private Map<String, String> attributes;
+
+    public Attributes() {
+        this(new HashMap<>());
+    }
+
+    private Attributes(Map<String, String> attributes) {
+        this.attributes = attributes;
+    }
+
+    public static Attributes from(Map<String, String> attributes) {
+        return new Attributes(attributes);
+    }
 
     public Attributes add(String key, String value) {
         attributes.put(key, value);
@@ -12,5 +26,18 @@ public class Attributes {
 
     public String get(String key) {
         return attributes.get(key);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Attributes that = (Attributes) o;
+        return Objects.equals(attributes, that.attributes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attributes);
     }
 }
