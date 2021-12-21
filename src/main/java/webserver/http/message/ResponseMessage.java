90daf5f837ed32af9b0bc314554afbc89611b89f
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-12-21 10:45:51 +0900

    fix(start-line): ResponseMessage에 StatusLine 추가

--- src/main/java/webserver/http/message/ResponseMessage.java
+++ src/main/java/webserver/http/message/ResponseMessage.java
@@ -3,20 +3,27 @@ package webserver.http.message;
 import webserver.Const;
 import webserver.http.Body;
 import webserver.http.header.ResponseHeader;
+import webserver.http.startline.StatusLine;
 
 import java.util.StringJoiner;
 
+import static webserver.http.header.Header.parseStartLine;
+
 public class ResponseMessage {
+
+    private StatusLine statusLine;
     private ResponseHeader header;
     private Body body;
 
-    public ResponseMessage(ResponseHeader header, Body body) {
+    public ResponseMessage(StatusLine statusLine, ResponseHeader header, Body body) {
+        this.statusLine = statusLine;
         this.header = header;
         this.body = body;
     }
 
     public static ResponseMessage from(String responseMessage) {
         String[] splittedResponseMessage = responseMessage.split(Const.CRLF + Const.CRLF);
+        StatusLine statusLine = StatusLine.from(parseStartLine(splittedResponseMessage[0]));
 
         StringJoiner body = new StringJoiner(Const.CRLF + Const.CRLF);
 
@@ -24,7 +31,11 @@ public class ResponseMessage {
             body.add(splittedResponseMessage[i]);
         }
 
-        return new ResponseMessage(ResponseHeader.from(splittedResponseMessage[0]), Body.from(body.toString()));
+        return new ResponseMessage(statusLine, ResponseHeader.from(splittedResponseMessage[0]), Body.from(body.toString()));
+    }
+
+    public StatusLine getStatusLine() {
+        return statusLine;
     }
 
     public ResponseHeader getHeader() {
